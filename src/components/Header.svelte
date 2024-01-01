49df0044189b629083ee7a10016d3323a95dc845
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2024-01-01 20:18:46 +0900

    done styling for header on tablet and desktop

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,7 +4,9 @@
 
 <header class="header">
 	<div class="header-wrapper">
-		<a href="/" class="header-link"><img src={logo} alt="header logo" width="80px" /></a>
+		<a href="/" class="header-logo"
+			><img class="header-logo-link" src={logo} alt="header logo" width="80px" /></a
+		>
 		<nav class="header-nav">
 			<ul class="nav-list">
 				<li class="nav-list-item"><a href="#">Features</a></li>
@@ -19,6 +21,7 @@
 	.header-wrapper {
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
 		padding-top: 24px;
 		width: min(88.27%, 1280px);
 		margin-right: auto;
@@ -35,5 +38,54 @@
 		text-decoration: none;
 		font-size: 0.75rem;
 		opacity: 0.8;
+	}
+
+	@media (768px < width <= 960px) {
+		.header-wrapper {
+			padding-top: 73px;
+		}
+		.header-logo-link {
+			width: 176px;
+		}
+		.header-logo-link:hover {
+			opacity: 0.8;
+			transition: ease-in 0.15s;
+		}
+		.nav-list {
+			gap: 57px;
+		}
+		.nav-list-item a {
+			font-size: 1rem;
+		}
+		.nav-list-item a:hover {
+			opacity: 1;
+			cursor: pointer;
+			font-weight: 700;
+			text-decoration-line: underline;
+			text-decoration-color: var(--pure-white);
+			transition: ease-in 0.1s;
+		}
+	}
+
+	@media (960px < width <= 1440px) {
+		.header-wrapper {
+			padding-top: 73px;
+		}
+		.header-logo-link {
+			width: 176px;
+		}
+		.nav-list {
+			gap: 57px;
+		}
+		.nav-list-item a {
+			font-size: 1rem;
+		}
+		.nav-list-item a:hover {
+			opacity: 1;
+			cursor: pointer;
+			text-decoration-line: underline;
+			text-decoration-color: var(--pure-white);
+			transition: ease-in 0.1s;
+		}
 	}
 </style>
